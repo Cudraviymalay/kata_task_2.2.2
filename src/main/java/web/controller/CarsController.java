e34e405b1c6ab4b172680962c74af1bd5b36df35
Mahmutov Azat <cudraviymalay@gmail.com>
2024-12-03 23:33:11 +0300

    еще немного добавлений, но всё-равно выдает ошибку хттп 500

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class CarController {
+public class CarsController {
 
-    @GetMapping("/Cars")
+    @GetMapping("/cars")
+    public String carsForSale () {
+        return "/cars";
+    }
 }
